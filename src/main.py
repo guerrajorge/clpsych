a71c0b76ea185bd3675f53ac6e942966562d75ca
guerrajorge <jorgej.guerra@gmail.com>
2019-03-28 10:45:25 -0400

    final comp models

--- src/main.py
+++ src/main.py
@@ -8,20 +8,6 @@ import os
 import csv
 
 
-def print_cube(num):
-    """
-    function to print cube of given num
-    """
-    print("Cube: {}".format(num * num * num))
-
-
-def print_square(num):
-    """
-    function to print square of given num
-    """
-    print("Square: {}".format(num * num))
-
-
 def post_sentiment(post, index, nlp):
     """
     for provided post, with one or multiple sentence, returns a sentiment distribution of the post by all the different
@@ -31,22 +17,27 @@ def post_sentiment(post, index, nlp):
     :param nlp: nlp library server object
     :return:
     """
+    # res = nlp.annotate(post,
+    #                    properties={'timeout': 60000000,
+    #                                'annotators': 'sentiment',
+    #                                'outputFormat': 'json'})
+    # # store the distribution of the 5 possible sentiments
+    # sentiment_dist = list()
+    #
+    # # obtain sentiment distribution for each sentence in the post
+    # for s in res["sentences"]:
+    #     sentiment_dist.append(s['sentimentDistribution'])
+    # # find average of the distributions by sentiment
+    # dist_sum = np.array(sentiment_dist).sum(axis=0)
+    # sentiment_dist = dist_sum / dist_sum.sum()
+    # # data point with index and averaged distribution
+    # row = np.append(index, sentiment_dist)
+    # return row
     res = nlp.annotate(post,
-                       properties={'timeout': 600000,
-                                   'annotators': 'sentiment',
+                       properties={'timeout': 60000000,
+                                   'annotators': 'ssplit',
                                    'outputFormat': 'json'})
-    # store the distribution of the 5 possible sentiments
-    sentiment_dist = list()
-
-    # obtain sentiment distribution for each sentence in the post
-    for s in res["sentences"]:
-        sentiment_dist.append(s['sentimentDistribution'])
-    # find average of the distributions by sentiment
-    dist_sum = np.array(sentiment_dist).sum(axis=0)
-    sentiment_dist = dist_sum / dist_sum.sum()
-    # data point with index and averaged distribution
-    row = np.append(index, sentiment_dist)
-    return row
+    return len(res['sentences'])
 
 
 def project_paths(project_name):
@@ -81,17 +72,43 @@ if __name__ == "__main__":
 
     project_path, data_path, model_path = project_paths(project_name='clpsych')
 
-    n = ''
-    input_filename = 'risk_title_body_{0}.csv'.format(n)
-    # this file will contains the risk, title, body and sentiment associated with each post
-    out_filename = 'risk_tbs_{0}.csv'.format(n)
-    out_filename_error = 'risk_tbs_{0}_error.csv'.format(n)
+    train_test = 'test'
+
+    n = '1'
+
+    # if train_test == 'train':
+    #
+    #     input_filename = 'risk_title_body_{0}.csv'.format(n)
+    #     # this file will contains the risk, title, body and sentiment associated with each post
+    #     out_filename = 'risk_tbs_{0}.csv'.format(n)
+    #     out_filename_error = 'risk_tbs_{0}_error.csv'.format(n)
+    #     out_file = Path(data_path, out_filename)
+    #     out_file_error = Path(data_path, out_filename_error)
+    #
+    #     print('reading data {0}'.format(input_filename))
+    #     suicide_data = pd.read_csv(Path(data_path, input_filename))
+    #     suicide_data.columns = ['index', 'risk_label', 'title_body']
+    #
+    # else:
+    #     data_path = Path(data_path, 'clpsych19_test_data')
+    #     input_filename = 'clpsych19_test_title_body_{0}.csv'.format(n)
+    #     out_filename = 'clpsych19_test_title_body_sentiments_sentence_{0}.csv'.format(n)
+    #     out_filename_error = 'clpsych19_test_title_body_sentiments_error_sentence_{0}.csv'.format(n)
+    #     out_file = Path(data_path, out_filename)
+    #     out_file_error = Path(data_path, out_filename_error)
+    #
+    #     print('reading data {0}'.format(input_filename))
+    #     suicide_data = pd.read_csv(Path(data_path, input_filename))
+
+    data_path = Path(data_path, 'clpsych19_test_data')
+    input_filename = 'clpsych19_test_title_body.csv'.format(n)
+    out_filename = 'clpsych19_test_title_body_sentiments_sentence.csv'.format(n)
+    out_filename_error = 'clpsych19_test_title_body_sentiments_error_sentence.csv'.format(n)
     out_file = Path(data_path, out_filename)
     out_file_error = Path(data_path, out_filename_error)
 
     print('reading data {0}'.format(input_filename))
     suicide_data = pd.read_csv(Path(data_path, input_filename))
-    suicide_data.columns = ['index', 'risk_label', 'title_body']
 
     # -1 for the first document which starts at 0
     last_index = -1
@@ -115,33 +132,31 @@ if __name__ == "__main__":
         writer_error = csv.writer(e_file, delimiter=',')
         for data_ix, data in suicide_data.iterrows():
             # make sure only new data points are being processed
-            if data['index'] > last_index:
-                print('processing {0}/{1}, file ix = {2}'.format(data_ix, total_len, data['index']))
+            if train_test == 'train':
+                c_last = data['user_id']
+            else:
+                c_last = data_ix
+
+            if data_ix > last_index:
+                print('processing {0}/{1}, file ix = {2}'.format(data_ix, total_len, c_last))
                 try:
-                    res = post_sentiment(post=data['title_body'], index=data['index'], nlp=nlp_object)
-                    writer.writerow(res)
+                    res = post_sentiment(post=data['title_body'], index=c_last, nlp=nlp_object)
+                    tmp_list = [data['user_id']]
+                    tmp_list.extend([data['post_id']])
+                    tmp_list.extend([res])
+                    # tmp_list.extend('0')
+                    writer.writerow(tmp_list)
                     o_file.flush()
                 except:
-                    print('error {0}'.format(data['index']))
-                    writer_error.writerow([data['index']])
+                    print('error {0}'.format(c_last))
+                    writer_error.writerow([c_last])
                     e_file.flush()
+                    tmp_list = [data['user_id']]
+                    tmp_list.extend([data['post_id']])
+                    tmp_list.extend([np.nan])
+                    # tmp_list.extend('1')
+                    writer.writerow(tmp_list)
+                    o_file.flush()
                     continue
 
     e_file.close()
-
-    # # creating thread
-    # t1 = threading.Thread(target=print_square, args=(10,))
-    # t2 = threading.Thread(target=print_cube, args=(10,))
-    #
-    # # starting thread 1
-    # t1.start()
-    # # starting thread 2
-    # t2.start()
-    #
-    # # wait until thread 1 is completely executed
-    # t1.join()
-    # # wait until thread 2 is completely executed
-    # t2.join()
-    #
-    # # both threads completely executed
-    # print("Done!")
--- src/model.py
+++ src/model.py
@@ -1,82 +1,286 @@
-import os
+import pandas as pd
 from pathlib import Path
 import sys
-import pandas as pd
+import os
 import numpy as np
-import re
-import collections
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-from sklearn.model_selection import train_test_split
-from nltk.corpus import stopwords
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils.np_utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
-from pycorenlp import StanfordCoreNLP
-from keras import models
-from keras import layers
-from keras import regularizers
-from utils.data_helpers import build_vocab
-from utils.data_helpers import load_data_and_labels
-from utils.data_helpers import pad_sentences
+from numpy import interp
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.linear_model import LogisticRegression
+import tensorflow as tf
+from keras import backend as k
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import multi_gpu_model
+from sklearn.impute import SimpleImputer
 
-project_name = 'clpsych'
-project_path = Path(os.getcwd()).parent
 
-if sys.platform == "win32":
-    data_path = 'D:\Dataset\{0}\dataset'.format(project_name)
-    model_path = Path(project_path, 'models')
-    src_path = src_path = Path(project_path, 'src')
+def class_report(y_true, y_pred, y_score=None, average='macro'):
+    if y_true.shape != y_pred.shape:
+        print("Error! y_true {0} is not the same shape as y_pred {1}".format(
+            y_true.shape,
+            y_pred.shape)
+        )
+        return
 
-elif sys.platform == 'darwin':
-    data_path = '/Volumes/Dataset/{0}/dataset'.format(project_name)
-    model_path = '/Volumes/Dataset/{0}/models'.format(project_name)
-    src_path = '/Volumes/Dataset/{0}/src'.format(project_name)
+    lb = LabelBinarizer()
 
-else:
-    data_path = Path(project_path, 'dataset')
-    model_path = Path(project_path, 'models')
-    src_path = Path(project_path, 'src')
+    if len(y_true.shape) == 1:
+        lb.fit(y_true)
 
-utils_path = str(Path(project_path, 'utils'))
-# including the project folder and the utils folder
-if utils_path not in ''.join(sys.path):
-    sys.path.extend([str(project_path), str(utils_path), str(src_path)])
+    # Value counts of predictions
+    labels, cnt = np.unique(
+        y_pred,
+        return_counts=True)
+    n_classes = len(labels)
+    pred_cnt = pd.Series(cnt, index=labels)
 
-print('project path = {0}'.format(project_path))
-print('data path = {0}'.format(data_path))
-print('model path = {0}'.format(model_path))
-print('utils path = {0}'.format(utils_path))
-print('sys.path = {0}'.format(sys.path))
+    metrics_summary = precision_recall_fscore_support(
+        y_true=y_true,
+        y_pred=y_pred,
+        labels=labels)
 
-n = 1
-filename = 'risk_title_body_{0}.csv'.format(n)
-suicide_data = pd.read_csv(Path(data_path, filename))
+    avg = list(precision_recall_fscore_support(
+        y_true=y_true,
+        y_pred=y_pred,
+        average=average))
 
-nlp = StanfordCoreNLP('http://localhost:9000')
+    metrics_sum_index = ['precision', 'recall', 'f1-score', 'support']
+    class_report_df = pd.DataFrame(
+        list(metrics_summary),
+        index=metrics_sum_index,
+        columns=labels)
 
-sentiment_list = list()
-sentiment_df = pd.DataFrame(columns=['text', 'sentiments', 'sentiment_dist', 'y'])
+    support = class_report_df.loc['support']
+    total = support.sum()
+    class_report_df['avg / total'] = avg[:-1] + [total]
 
-total_len = suicide_data.shape[0]
+    class_report_df = class_report_df.T
+    class_report_df['pred-cnt'] = pred_cnt
+    class_report_df['pred-cnt'].iloc[-1] = total
 
-print('filename = {0}'.format(filename))
+    if not (y_score is None):
+        # false positive rate
+        fpr = dict()
+        # true positive rate
+        tpr = dict()
+        roc_auc = dict()
+        for label_ix, label in enumerate(labels):
+            fpr[label], tpr[label], _ = roc_curve(
+                (y_true == label).astype(int),
+                y_score[:, label_ix])
 
-for ix, post in suicide_data.iterrows():
-    print('processing {0}/{1}'.format(ix, total_len))
-    res = nlp.annotate(post['title_body'],
-                       properties={'timeout': 60000000, 'annotators': 'sentiment', 'outputFormat': 'json'})
-    sentiment = list()
-    sentiment_dist = list()
-    for s in res["sentences"]:
-        # print("{0}: {1}: {2} {3}".format( s["index"], " ".join([t["word"] for t in s["tokens"]]),
-        # s["sentimentValue"], s["sentiment"]))
-        sentiment.append(s["sentiment"])
-        sentiment_dist.append(s['sentimentDistribution'])
-    sentiment_df = sentiment_df.append(
-        {'text': post['title_body'], 'sentiments': sentiment, 'sent dist': sentiment_dist,
-         'y': post['risk_label']}, ignore_index=True)
+            roc_auc[label] = auc(fpr[label], tpr[label])
 
-filename = 'risk_title_body_{0}_results.csv'.format(n)
-sentiment_df.to_csv(Path(data_path, filename), index=False)
+        if average == 'micro':
+            if n_classes <= 2:
+                fpr["avg / total"], tpr["avg / total"], _ = roc_curve(
+                    lb.transform(y_true).ravel(),
+                    y_score[:, 1].ravel())
+            else:
+                fpr["avg / total"], tpr["avg / total"], _ = roc_curve(
+                    lb.transform(y_true).ravel(),
+                    y_score.ravel())
+
+            roc_auc["avg / total"] = auc(
+                fpr["avg / total"],
+                tpr["avg / total"])
+
+        elif average == 'macro':
+            # First aggregate all false positive rates
+            all_fpr = np.unique(np.concatenate([
+                fpr[i] for i in labels]
+            ))
+
+            # Then interpolate all ROC curves at this points
+            mean_tpr = np.zeros_like(all_fpr)
+            for i in labels:
+                mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+            # Finally average it and compute AUC
+            mean_tpr /= n_classes
+
+            fpr["macro"] = all_fpr
+            tpr["macro"] = mean_tpr
+
+            roc_auc["avg / total"] = auc(fpr["macro"], tpr["macro"])
+
+        class_report_df['AUC'] = pd.Series(roc_auc)
+
+    return class_report_df
+
+
+def f1(y_true, y_prediction):
+
+    y_prediction = k.round(y_prediction)
+    tp = k.sum(k.cast(y_true * y_prediction, 'float'), axis=0)
+    fp = k.sum(k.cast((1 - y_true) * y_prediction, 'float'), axis=0)
+    fn = k.sum(k.cast(y_true * (1 - y_prediction), 'float'), axis=0)
+
+    p = tp / (tp + fp + k.epsilon())
+    r = tp / (tp + fn + k.epsilon())
+
+    f1_metric = 2 * p * r / (p + r + k.epsilon())
+    f1_metric = tf.where(tf.is_nan(f1_metric), tf.zeros_like(f1_metric), f1_metric)
+
+    return k.mean(f1_metric)
+
+
+def f1_loss(y_true, y_prediction):
+
+    tp = k.sum(k.cast(y_true * y_prediction, 'float'), axis=0)
+    fp = k.sum(k.cast((1 - y_true) * y_prediction, 'float'), axis=0)
+    fn = k.sum(k.cast(y_true * (1 - y_prediction), 'float'), axis=0)
+
+    p = tp / (tp + fp + k.epsilon())
+    r = tp / (tp + fn + k.epsilon())
+
+    f1_val = 2 * p * r / (p + r + k.epsilon())
+    f1_val = tf.where(tf.is_nan(f1_val), tf.zeros_like(f1_val), f1_val)
+
+    return 1 - k.mean(f1_val)
+
+
+def project_paths(project_name):
+    """
+    obtain the necessary directory paths
+    :param project_name: name of the project
+    :return: project_path, data_loc, model_loc
+    """
+    project_loc = Path(os.getcwd()).parent
+
+    if sys.platform == "win32":
+        data_loc = 'D:\Dataset\{0}\dataset'.format(project_name)
+        model_loc = Path(project_loc, 'models')
+
+    elif sys.platform == 'darwin':
+        data_loc = '/Volumes/Dataset/{0}/dataset'.format(project_name)
+        model_loc = '/Volumes/Dataset/{0}/models'.format(project_name)
+
+    else:
+        data_loc = Path(project_loc, 'dataset')
+        model_loc = Path(project_loc, 'models')
+
+    utils_path = str(Path(project_loc, 'utils'))
+    # including the project folder and the utils folder
+    if utils_path not in ''.join(sys.path):
+        sys.path.extend([str(project_loc), str(utils_path)])
+
+    return project_loc, data_loc, model_loc
+
+
+def run_model(data, classifier, k_fold):
+
+    data = data.fillna(-1).copy()
+
+    random_state = 7
+    report_with_auc_df = ''
+
+    # keep non control user ids
+    k_fold = k_fold[k_fold['user_id'] > 0]
+
+    print('5 fold CV starting')
+
+    for fold_ix in range(1, 6):
+
+        print('\nFold = {0}'.format(fold_ix))
+
+        train_ix = k_fold[(k_fold['fold'] == fold_ix) & (k_fold['train_text'] == 'training')]['user_id']
+        test_ix = k_fold[(k_fold['fold'] == fold_ix) & (k_fold['train_text'] == 'test')]['user_id']
+
+        x_train = data[data.index.isin(train_ix)].copy()
+        x_test = data[data.index.isin(test_ix)].copy()
+
+        y_train = x_train['target']
+        x_train.drop(['target'], axis=1, inplace=True)
+
+        y_test = x_test['target']
+        x_test.drop(['target'], axis=1, inplace=True)
+
+        imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+        imp_mean.fit(x_train)
+        x_train = imp_mean.transform(x_train)
+
+        imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+        imp_mean.fit(x_test)
+        x_test = imp_mean.transform(x_test)
+
+        x_train = (x_train - x_train.mean()) / (x_train.max() - x_train.min())
+        x_test = (x_test - x_test.mean()) / (x_test.max() - x_test.min())
+
+        if classifier == 'nn':
+
+            available_gpu = k.tensorflow_backend._get_available_gpus()
+            available_gpus = len(available_gpu)
+
+            # basic neural network
+            model = Sequential()
+            model.add(Dense(5000, input_dim=np.shape(x_train)[1], activation='relu'))
+            model.add(Dense(2500, activation='relu'))
+            model.add(Dense(2000, activation='relu'))
+            model.add(Dense(1000, activation='relu'))
+            model.add(Dense(500, activation='relu'))
+            model.add(Dense(250, activation='relu'))
+            model.add(Dense(100, activation='relu'))
+            model.add(Dense(50, activation='relu'))
+            model.add(Dense(len(y_train.unique()), activation='sigmoid'))
+
+            model = multi_gpu_model(model, gpus=available_gpus)
+
+            # treating every instance of class 1 as 50 instances of class 0
+            class_weight = {1: 1, 2: 10, 3: 10, 4: 1}
+
+            # Compile model
+            model.compile(optimizer='adam', loss=f1_loss, metrics=[f1])
+            model.fit(x_train, y_train, epochs=100, batch_size=100, verbose=1,
+                      class_weight=class_weight)
+
+            y_pred_train = np.argmax(model.predict(x_train), axis=1)
+            y_pred_test = np.argmax(model.predict(x_test), axis=1)
+            y_score = model.predict(x_test)
+
+        elif classifier == 'logit':
+
+            model = LogisticRegression(class_weight='balanced', n_jobs=-1,
+                                       multi_class='auto', solver='lbfgs',
+                                       tol=0.00001, C=10, max_iter=1000, verbose=True,
+                                       random_state=random_state)
+            model.fit(x_train, y_train)
+            y_pred_test = model.predict(x_test)
+            y_score = model.predict_proba(x_test)
+
+        report_with_auc = class_report(
+            y_true=y_test,
+            y_pred=y_pred_test,
+            y_score=y_score,
+            average='macro')
+
+        cv_column = [fold_ix]
+        cv_column.extend([''] * (report_with_auc.index.shape[0] - 1))
+        report_with_auc['Fold'] = cv_column
+        report_with_auc['Risk-Factor'] = report_with_auc.index
+        report_with_auc = report_with_auc.set_index(['Fold', 'Risk-Factor'])
+
+        if fold_ix == 1:
+            report_with_auc_df = report_with_auc.copy()
+        else:
+            report_with_auc_df = report_with_auc_df.append(report_with_auc.copy())
+
+        return report_with_auc_df
+
+
+if __name__ == "__main__":
+
+    project_path, data_path, model_path = project_paths(project_name='clpsych')
+
+    filename = Path(data_path, 'static_features_pandas_v2.pkl')
+    dataset = pd.read_pickle(filename)
+
+    # define 5-fold cross validation test harness
+    folds = pd.read_csv(Path(data_path, 'clpsych19_public_crossvalidation_splits.csv'), header=None,
+                        names=['fold', 'train_text', 'user_id'])
+
+    report = run_model(data=dataset, classifier='logit', k_fold=folds)
+
+    report.to_csv(Path(data_path, 'report_logit.csv'))
